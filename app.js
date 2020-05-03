5ced4ecd1eab96abd2f7a47931b2f8b5a22a9942
Amir.E Haji Hussaini <niamileo@gmail.com>
2020-05-03 14:25:19 +0430

    user table validation added

--- app.js
+++ app.js
@@ -61,7 +61,8 @@ app.use((error, req, res, next) => {
 	console.log(error);
 	const status = error.statusCode || 500; // Default value will be 500
 	const message = error.message;
-	res.status(status).json({ message: message });
+	const data = error.data;
+	res.status(status).json({ message: message, data: data });
 });
 
 // Table Associations
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,5 +1,34 @@
 const express = require("express");
+const { body } = require("express-validator");
+
+const User = require("../../models/user");
+
+const apiAuthController = require("../../api/auth");
 
 const router = express.Router();
+
+router.put(
+	"/signup",
+	[
+		body("email")
+			.isEmail()
+			.withMessage("Please enter a valid email number.")
+			.custom((value, { req }) => {
+				return User.findOne({
+					where: { email: value },
+				}).then((userDoc) => {
+					if (userDoc) {
+						return Promise.reject("E-mail already exists; please pick another one.");
+					}
+				});
+			}),
+		body("password", "password length should be at least, 6 characters.")
+			.isLength({ min: 6 })
+			.trim()
+			.not()
+			.isEmpty(),
+	],
+	apiAuthController.signup
+);
 
 module.exports = router;
