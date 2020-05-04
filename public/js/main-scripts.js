1ad8301fa5947a4de05738491c9d78f7ccd7fa7b
Amir.E Haji Hussaini <niamileo@gmail.com>
2020-05-04 14:13:39 +0430

    login ajax added

--- public/js/main-scripts.js
+++ public/js/main-scripts.js
@@ -1,7 +1,38 @@
 $(function () {
+	var validator = $("#loginForm").validate();
 	// Resize $ Change Background on Scroll
 	$(document).scroll(function () {
 		var $nav = $(".navbar-fixed-top");
 		$nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+	});
+
+	////////////////////////////////////////
+	///////////// REGISTER /////////////////
+	////////////////////////////////////////
+
+	// Login User
+	$("#loginBtn").on("click", function (e) {
+		e.preventDefault();
+		var form = $("#loginForm")[0];
+		var formData = new FormData(form);
+		if ($("#loginForm").valid()) {
+			$.ajax({
+				url: "http://localhost:3000/auth/login",
+				type: "POST",
+				processData: false,
+				contentType: false,
+				data: formData,
+				success: function (result) {
+					console.log(result);
+					localStorage.setItem("token", result.token);
+				},
+			});
+		}
+	});
+
+	// Clicking on Login-in - Sign-up Button
+	$("#registerBtn").on("click", function () {
+		validator.resetForm();
+		$("#loginForm")[0].trigger("reset");
 	});
 });
